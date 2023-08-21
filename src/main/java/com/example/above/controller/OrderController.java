f83d06975188d9e882568510046a437c2188831f
kwon <kyjyj6334@gmail.com>
2023-08-21 17:21:55 +0900

    주문 로그인

--- src/main/java/com/example/above/controller/OrderController.java
+++ src/main/java/com/example/above/controller/OrderController.java
@@ -24,10 +24,44 @@ public class OrderController {
     @Autowired
     OrderService svc;
 
+    // @GetMapping("/order")
+    // public String checkIDsession(Model model, HttpSession session, @RequestParam("prodNum") int prodNum,@RequestParam("cnt") int cnt ) {
+    //     String id = (String) session.getAttribute("id");
+    //     System.out.println("prodNum: "+prodNum);
+    //     System.out.println("cnt: "+cnt);
+
+    //     MemVO memVO = svc.selectMeminfo(id);
+
+    //     String mb =memVO.getMemMobile();
+    //     String[] mobile = mb.split("/");
+    //     memVO.setMemMobile1(mobile[1]);
+    //     memVO.setMemMobile2(mobile[2]);
+    //     memVO.setMemMobile3(mobile[3]);
+
+    //     String ad = memVO.getMemAddr();
+    //     String [] addr = ad.split("/");
+    //     memVO.setMemAddr1(addr[0]);
+    //     memVO.setMemAddr2(addr[1]);
+    //     memVO.setMemAddr3(addr[2]);
+    //     memVO.setMemAddr4(addr[3]); //addr4의 유무에 따라 오류 발생 ㅜㅜ
+
+    //     ProdVO ProdVO = svc.selectOrdProd(prodNum);
+        
+    //     model.addAttribute("memVO", memVO);
+    //     model.addAttribute("ProdVO", ProdVO);
+    //     model.addAttribute("ordCnt", cnt);
+    //     return "prodOrder";
+    // }  
+
     @GetMapping("/order")
     public String selectOrdMem(Model model, HttpSession session, @RequestParam("prodNum") int prodNum,@RequestParam("cnt") int cnt ) {
         String id = (String) session.getAttribute("id");
-        System.out.println("prodNum: "+prodNum);
+
+        if (id == null) {
+            model.addAttribute("message", "로그인이 필요합니다.");
+            // return "login";
+        } else {
+            System.out.println("prodNum: "+prodNum);
         System.out.println("cnt: "+cnt);
 
         MemVO memVO = svc.selectMeminfo(id);
@@ -50,6 +84,10 @@ public class OrderController {
         model.addAttribute("memVO", memVO);
         model.addAttribute("ProdVO", ProdVO);
         model.addAttribute("ordCnt", cnt);
+        
+            
+        }
+
         return "prodOrder";
     }  
 
--- src/main/webapp/WEB-INF/prodOrder.jsp
+++ src/main/webapp/WEB-INF/prodOrder.jsp
@@ -1,5 +1,6 @@
 
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html>
 <html>
@@ -20,6 +21,13 @@
 <body>
 
 <section>
+
+	<c:if test="${not empty message}">
+    <script>
+        alert('${message}');
+        history.go(-1);
+    </script>
+	</c:if>
 
 		<div id="shopping_buy_wrap" >
 			<div id="s_order" >
