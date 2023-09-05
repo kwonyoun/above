bd4c34af0b0488b825beeaa4862a8b02ecf7c45b
kwon <kyjyj6334@gmail.com>
2023-09-05 11:05:16 +0900

    vo어노테이션 수정

--- src/main/java/com/example/above/vo/OrdinfoVO.java
+++ src/main/java/com/example/above/vo/OrdinfoVO.java
@@ -2,9 +2,13 @@ package com.example.above.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
+// @Data
+// @AllArgsConstructor
+@Getter
+@Setter
 public class OrdinfoVO {
 
     private int ordNum;
@@ -21,7 +25,10 @@ public class OrdinfoVO {
     private String ordProdNum;
     private int ordQuan;
     
-    // private String prodImg;
+    private String prodImg;
     //inner join으로 select하려고 OrdinfoVO에 string만들었는데 이거때문에 배열 오류났음.
     //그러면 주문내역에서 prodImg는 어떻게 가져와야 될까?
+    //@Data 어노테이션 대신 @Getter. @Setter 로 썻더니 된다.
+    //@Data 어노테이션을 지양해야한다.
+    //@ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor을 모두 포함하는 강력한 어노테이션
 }
